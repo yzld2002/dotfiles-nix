4d4b0b90f9257b77be52c55bc7c55bab7ce7c40c
Zhichao <Zhichao.lin@landing.ai>
2023-12-16 17:45:42 +0800

    remove unused files

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -3,10 +3,6 @@
 with lib;
 let cfg = 
     config.modules.packages;
-    screen = pkgs.writeShellScriptBin "screen" ''${builtins.readFile ./screen}'';
-    bandw = pkgs.writeShellScriptBin "bandw" ''${builtins.readFile ./bandw}'';
-    maintenance = pkgs.writeShellScriptBin "maintenance" ''${builtins.readFile ./maintenance}'';
-
 in {
     options.modules.packages = { enable = mkEnableOption "packages"; };
     config = mkIf cfg.enable {
@@ -18,9 +14,8 @@ in {
             grim slurp slop
             imagemagick age libnotify
             git python3 lua zig 
-            mpv firefox pqiv
-            screen bandw maintenance
-            wf-recorder anki-bin 
+            mpv chromium pqiv
+            wf-recorder
         ];
     };
 }
