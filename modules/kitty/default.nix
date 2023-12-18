ea2ccd56403b18eec1dd6260f7186adb494e82e9
Zhichao <Zhichao.lin@landing.ai>
2023-12-18 21:53:38 +0800

    kitty

--- modules/kitty/default.nix
+++ modules/kitty/default.nix
@@ -6,9 +6,15 @@ let cfg = config.modules.kitty;
 in {
     options.modules.kitty = { enable = mkEnableOption "wofi"; };
     config = mkIf cfg.enable {
-        programs.kitty = {
-            enable = true;
+      programs.kitty = {
+        enable = true;
+        font = {
+          name = "CaskaydiaCoveNerdFont";
+          font = 16;
         };
+        shellIntegration.enableZshIntegration = true;
+        theme = "One Half Dark";
+      };
     };
 }
 
