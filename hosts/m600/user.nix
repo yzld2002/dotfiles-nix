01c2257af43a1eeb6ca2c443bd6c23c285a9d071
Zhichao <Zhichao.lin@landing.ai>
2023-12-19 20:47:10 +0800

    clean unused

--- hosts/m600/user.nix
+++ hosts/m600/user.nix
@@ -4,22 +4,17 @@
   imports = [ ../../modules/default.nix ];
   config.modules = {
     # gui
-    eww.enable = true;
-    dunst.enable = true;
     hyprland.enable = true;
-    wofi.enable = true;
     kitty.enable = true;
     gui.enable = true;
 
     # cli
-    nvim.enable = true;
     zsh.enable = true;
     git.enable = true;
     gpg.enable = true;
     direnv.enable = true;
 
     # system
-    xdg.enable = true;
     packages.enable = true;
   };
 }
