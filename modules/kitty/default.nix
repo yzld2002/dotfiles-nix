3bd5a6cdd88a3801b335daf3bbd0c8f58380b99b
Zhichao <Zhichao.lin@landing.ai>
2023-12-19 20:04:33 +0800

    lint

--- modules/kitty/default.nix
+++ modules/kitty/default.nix
@@ -4,17 +4,17 @@ with lib;
 let cfg = config.modules.kitty;
 
 in {
-    options.modules.kitty = { enable = mkEnableOption "wofi"; };
-    config = mkIf cfg.enable {
-      programs.kitty = {
-        enable = true;
-        font = {
-          name = "CaskaydiaCoveNerdFont";
-          size = 16;
-        };
-        shellIntegration.enableZshIntegration = true;
-        theme = "One Half Dark";
+  options.modules.kitty = { enable = mkEnableOption "wofi"; };
+  config = mkIf cfg.enable {
+    programs.kitty = {
+      enable = true;
+      font = {
+        name = "CaskaydiaCoveNerdFont";
+        size = 16;
       };
+      shellIntegration.enableZshIntegration = true;
+      theme = "One Half Dark";
     };
+  };
 }
 
