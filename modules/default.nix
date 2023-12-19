bb79ca88a81596d433f6b52b17879a040ed27cbf
Zhichao <Zhichao.lin@landing.ai>
2023-12-19 20:46:12 +0800

    remove unused packages

--- modules/default.nix
+++ modules/default.nix
@@ -4,22 +4,17 @@
   home.stateVersion = "21.03";
   imports = [
     # gui
-    ./eww
-    ./dunst
     ./hyprland
-    ./wofi
     ./kitty
     ./gui
 
     # cli
-    ./nvim
     ./zsh
     ./git
     ./gpg
     ./direnv
 
     # system
-    ./xdg
     ./packages
   ];
 }
