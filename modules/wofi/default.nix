44e24c3df37580083ed563d9a7ef15ddb719d1bb
Zhichao <Zhichao.lin@landing.ai>
2023-12-19 20:05:02 +0800

    lint

--- modules/wofi/default.nix
+++ modules/wofi/default.nix
@@ -4,8 +4,8 @@ with lib;
 let cfg = config.modules.wofi;
 
 in {
-    options.modules.wofi = { enable = mkEnableOption "wofi"; };
-    config = mkIf cfg.enable {
-        home.file.".config/wofi.css".source = ./wofi.css;
-    };
+  options.modules.wofi = { enable = mkEnableOption "wofi"; };
+  config = mkIf cfg.enable {
+    home.file.".config/wofi.css".source = ./wofi.css;
+  };
 }
