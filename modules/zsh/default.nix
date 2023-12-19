823491fff39f0f81bdf88124174a817900a12383
Zhichao <Zhichao.lin@landing.ai>
2023-12-19 13:27:53 +0800

    fix zsh

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -15,7 +15,7 @@ in {
     programs.zsh = {
       enable = true;
       enableAutosuggestions = true;
-      enableSyntaxHighlighting = true;
+      syntaxHighlighting = { enable = true; };
       oh-my-zsh = {
         enable = true;
         plugins = [ "z" "extract" ];
