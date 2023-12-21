51b0c4d43197034525f35c376a722d5fbde78c34
Zhichao <Zhichao.lin@landing.ai>
2023-12-21 21:10:39 +0800

    font config

--- modules/system/fonts/default.nix
+++ modules/system/fonts/default.nix
@@ -12,233 +12,205 @@
     ];
     fontconfig = {
       localConf = ''
-        <?xml version="1.0"?>
-        <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
-        <fontconfig>
-          <!-- Default system-ui fonts -->
-          <match target="pattern">
-            <test name="family">
-              <string>system-ui</string>
-            </test>
-            <edit name="family" mode="prepend" binding="strong">
-              <string>sans-serif</string>
-            </edit>
-          </match>
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+    <!--字体设定 -->
+    <match target="font">
+        <edit name="embeddedbitmap" mode="assign">
+            <bool>false</bool>
+        </edit>
+    </match>
+    <match>
+        <test qual="any" name="family">
+            <string>serif</string>
+        </test>
+        <edit name="family" mode="prepend" binding="strong">
+            <string>更纱黑体 UI SC</string>
+        </edit>
+    </match>
+    <match target="pattern">
+        <test qual="any" name="family">
+            <string>sans-serif</string>
+        </test>
+        <edit name="family" mode="prepend" binding="strong">
+            <string>更纱黑体 UI SC</string>
+        </edit>
+    </match>
+    <match target="pattern">
+        <test qual="any" name="family">
+            <string>monospace</string>
+        </test>
+        <edit name="family" mode="prepend" binding="strong">
+            <string>等距更纱黑体 SC</string>
+        </edit>
+    </match>
+    <match>
+        <test name="lang" compare="contains">
+            <string>zh</string>
+        </test>
+        <test name="family">
+            <string>serif</string>
+        </test>
+        <edit name="family" mode="prepend">
+            <string>更纱黑体 UI SC</string>
+        </edit>
+    </match>
+    <match>
+        <test name="lang" compare="contains">
+            <string>zh</string>
+        </test>
+        <test name="family">
+            <string>sans-serif</string>
+        </test>
+        <edit name="family" mode="prepend">
+            <string>更纱黑体 UI SC</string>
+        </edit>
+    </match>
+    <match>
+        <test name="lang" compare="contains">
+            <string>zh</string>
+        </test>
+        <test name="family">
+            <string>monospace</string>
+        </test>
+        <edit name="family" mode="prepend">
+            <string>等距更纱黑体 SC</string>
+        </edit>
+    </match>
 
-          <!-- Default sans-serif fonts-->
-          <match target="pattern">
-            <test name="family">
-              <string>sans-serif</string>
-            </test>
-            <edit name="family" mode="prepend" binding="strong">
-              <string>Noto Sans CJK SC</string>
-              <string>Noto Sans</string>
-              <string>Twemoji</string>
-            </edit>
-          </match>
+    <!--字体替换 -->
+    <match target="pattern">
+        <test qual="any" name="family">
+            <string>Noto Sans CJK SC</string>
+        </test>
+        <edit name="family" mode="assign" binding="same">
+            <string>更纱黑体 UI SC</string>
+        </edit>
+    </match>
 
-          <!-- Default serif fonts-->
-          <match target="pattern">
-            <test name="family">
-              <string>serif</string>
-            </test>
-            <edit name="family" mode="prepend" binding="strong">
-              <string>Noto Serif CJK SC</string>
-              <string>Noto Serif</string>
-              <string>Twemoji</string>
-            </edit>
-          </match>
+    <match target="pattern">
+        <test qual="any" name="family">
+            <string>文泉驿微米黑</string>
+        </test>
+        <edit name="family" mode="assign" binding="same">
+            <string>更纱黑体 UI SC</string>
+        </edit>
+    </match>
 
-          <!-- Default monospace fonts-->
-          <match target="pattern">
-            <test name="family">
-              <string>monospace</string>
-            </test>
-            <edit name="family" mode="prepend" binding="strong">
-              <string>Noto Sans Mono CJK SC</string>
-              <string>Symbols Nerd Font</string>
-              <string>Twemoji</string>
-            </edit>
-          </match>
+    <match target="pattern">
+        <test qual="any" name="family">
+            <string>SimSun</string>
+        </test>
+        <edit name="family" mode="assign" binding="same">
+            <string>更纱黑体 UI SC</string>
+        </edit>
+    </match>
 
-          <match target="pattern">
-            <test name="lang">
-              <string>zh-HK</string>
-            </test>
-            <test name="family">
-              <string>Noto Sans CJK SC</string>
-            </test>
-            <edit name="family" binding="strong">
-              <string>Noto Sans CJK HK</string>
-            </edit>
-          </match>
+    <match target="pattern">
+        <test qual="any" name="family">
+            <string>SimHei</string>
+        </test>
+        <edit name="family" mode="assign" binding="same">
+            <string>更纱黑体 UI SC</string>
+        </edit>
+    </match>
 
-          <match target="pattern">
-            <test name="lang">
-              <string>zh-HK</string>
-            </test>
-            <test name="family">
-              <string>Noto Serif CJK SC</string>
-            </test>
-            <edit name="family" binding="strong">
-              <!-- not have HK -->
-              <string>Noto Serif CJK TC</string>
-            </edit>
-          </match>
+    <match target="pattern">
+        <test qual="any" name="family">
+            <string>Microsoft Yahei</string>
+        </test>
+        <edit name="family" mode="assign" binding="same">
+            <string>更纱黑体 UI SC</string>
+        </edit>
+    </match>
 
-          <match target="pattern">
-            <test name="lang">
-              <string>zh-HK</string>
-            </test>
-            <test name="family">
-              <string>Noto Sans Mono CJK SC</string>
-            </test>
-            <edit name="family" binding="strong">
-              <string>Noto Sans Mono CJK HK</string>
-            </edit>
-          </match>
+    <match target="pattern">
+        <test qual="any" name="family">
+            <string>SimKai</string>
+        </test>
+        <edit name="family" mode="assign" binding="same">
+            <string>更纱黑体 UI SC</string>
+        </edit>
+    </match>
 
-          <match target="pattern">
-            <test name="lang">
-              <string>zh-TW</string>
-            </test>
-            <test name="family">
-              <string>Noto Sans CJK SC</string>
-            </test>
-            <edit name="family" binding="strong">
-              <string>Noto Sans CJK TC</string>
-            </edit>
-          </match>
+    <match target="pattern">
+        <test qual="any" name="family">
+            <string>宋体</string>
+        </test>
+        <edit name="family" mode="assign" binding="same">
+            <string>更纱黑体 UI SC</string>
+        </edit>
+    </match>
 
-          <match target="pattern">
-            <test name="lang">
-              <string>zh-TW</string>
-            </test>
-            <test name="family">
-              <string>Noto Serif CJK SC</string>
-            </test>
-            <edit name="family" binding="strong">
-              <string>Noto Serif CJK TC</string>
-            </edit>
-          </match>
+    <match target="pattern">
+        <test qual="any" name="family">
+            <string>楷体</string>
+        </test>
+        <edit name="family" mode="assign" binding="same">
+            <string>更纱黑体 UI SC</string>
+        </edit>
+    </match>
 
-          <match target="pattern">
-            <test name="lang">
-              <string>zh-TW</string>
-            </test>
-            <test name="family">
-              <string>Noto Sans Mono CJK SC</string>
-            </test>
-            <edit name="family" binding="strong">
-              <string>Noto Sans Mono CJK TC</string>
-            </edit>
-          </match>
+    <match target="pattern">
+        <test qual="any" name="family">
+            <string>黑体</string>
+        </test>
+        <edit name="family" mode="assign" binding="same">
+            <string>更纱黑体 UI SC</string>
+        </edit>
+    </match>
 
-          <match target="pattern">
-            <test name="lang">
-              <string>ja</string>
-            </test>
-            <test name="family">
-              <string>Noto Sans CJK SC</string>
-            </test>
-            <edit name="family" binding="strong">
-              <string>Noto Sans CJK JP</string>
-            </edit>
-          </match>
+    <match target="pattern">
+        <test qual="any" name="family">
+            <string>微软雅黑</string>
+        </test>
+        <edit name="family" mode="assign" binding="same">
+            <string>更纱黑体 UI SC</string>
+        </edit>
+    </match>
 
-          <match target="pattern">
-            <test name="lang">
-                <string>ja</string>
-            </test>
-            <test name="family">
-              <string>Noto Serif CJK SC</string>
-            </test>
-            <edit name="family" binding="strong">
-              <string>Noto Serif CJK JP</string>
-            </edit>
-          </match>
-
-          <match target="pattern">
-            <test name="lang">
-              <string>ja</string>
-            </test>
-            <test name="family">
-              <string>Noto Sans Mono CJK SC</string>
-            </test>
-            <edit name="family" binding="strong">
-              <string>Noto Sans Mono CJK JP</string>
-            </edit>
-          </match>
-
-          <match target="pattern">
-            <test name="lang">
-              <string>ko</string>
-            </test>
-            <test name="family">
-              <string>Noto Sans CJK SC</string>
-            </test>
-            <edit name="family" binding="strong">
-              <string>Noto Sans CJK KR</string>
-            </edit>
-          </match>
-
-          <match target="pattern">
-            <test name="lang">
-              <string>ko</string>
-            </test>
-            <test name="family">
-              <string>Noto Serif CJK SC</string>
-            </test>
-            <edit name="family" binding="strong">
-              <string>Noto Serif CJK KR</string>
-            </edit>
-          </match>
-
-          <match target="pattern">
-            <test name="lang">
-              <string>ko</string>
-            </test>
-            <test name="family">
-              <string>Noto Sans Mono CJK SC</string>
-            </test>
-            <edit name="family" binding="strong">
-              <string>Noto Sans Mono CJK KR</string>
-            </edit>
-          </match>
-
-          <!-- Replace monospace fonts -->
-          <match target="pattern">
-            <test name="family" compare="contains">
-              <string>Source Code</string>
-            </test>
-            <edit name="family" binding="strong">
-              <string>Iosevka Term</string>
-            </edit>
-          </match>
-          <match target="pattern">
-            <test name="lang" compare="contains">
-              <string>en</string>
-            </test>
-            <test name="family" compare="contains">
-              <string>Noto Sans CJK</string>
-            </test>
-            <edit name="family" mode="prepend" binding="strong">
-              <string>Noto Sans</string>
-            </edit>
-          </match>
-
-          <match target="pattern">
-            <test name="lang" compare="contains">
-              <string>en</string>
-            </test>
-            <test name="family" compare="contains">
-              <string>Noto Serif CJK</string>
-            </test>
-            <edit name="family" mode="prepend" binding="strong">
-              <string>Noto Serif</string>
-            </edit>
-          </match>
-        </fontconfig>
+    <match target="pattern">
+        <test qual="any" name="family">
+            <string>Noto Mono</string>
+        </test>
+        <edit name="family" mode="assign" binding="same">
+            <string>等距更纱黑体 SC</string>
+        </edit>
+    </match>
+    <match target="pattern">
+        <test qual="any" name="family">
+            <string>Noto Sans Mono</string>
+        </test>
+        <edit name="family" mode="assign" binding="same">
+            <string>等距更纱黑体 SC</string>
+        </edit>
+    </match>
+    <match target="pattern">
+        <test qual="any" name="family">
+            <string>Monospace</string>
+        </test>
+        <edit name="family" mode="assign" binding="same">
+            <string>等距更纱黑体 SC</string>
+        </edit>
+    </match>
+    <match target="pattern">
+        <test qual="any" name="family">
+            <string>Ubuntu Mono</string>
+        </test>
+        <edit name="family" mode="assign" binding="same">
+            <string>等距更纱黑体 SC</string>
+        </edit>
+    </match>
+    <match target="pattern">
+        <test qual="any" name="family">
+            <string>Noto Sans Mono CJK SC</string>
+        </test>
+        <edit name="family" mode="assign" binding="same">
+            <string>等距更纱黑体 SC</string>
+        </edit>
+    </match>
+</fontconfig>
       '';
     };
   };
