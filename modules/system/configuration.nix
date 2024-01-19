d3bca387183fef956a36ba423701af8da5e3ed39
yzld2002 <yzld2002@gmail.com>
2024-01-19 20:55:03 +0800

    minor indent

--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -18,7 +18,7 @@
       enable = true;
       extraPortals = with pkgs; [
         xdg-desktop-portal-wlr
-          xdg-desktop-portal-gtk
+        xdg-desktop-portal-gtk
       ];
       config.common.default = "*";
     };
