984f7c9f5995fe3c0e7823d901b4c40dc5b49e38
yzld2002 <yzld2002@gmail.com>
2024-01-27 15:07:08 +0800

    change source

--- flake.nix
+++ flake.nix
@@ -2,11 +2,10 @@
   description = "NixOS Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # Home manager kde
