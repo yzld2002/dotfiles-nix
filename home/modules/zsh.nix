3cfd580ab5e2ecf2748f13ee910eb5571e55ce8c
yzld2002 <yzld2002@gmail.com>
2024-01-28 16:14:42 +0800

    fix zsh

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -1,34 +1,30 @@
 {pkgs, ...}: {
   # ZSH stuff
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+  };
   programs.zsh = {
     enable = true;
-
-    programs.starship = {
+    enableAutosuggestions = true;
+    syntaxHighlighting = { enable = true; };
+    oh-my-zsh = {
       enable = true;
-      enableZshIntegration = true;
+      plugins = [ "z" "extract" "fd" "fzf" "git" ];
     };
-    programs.zsh = {
-      enable = true;
-      enableAutosuggestions = true;
-      syntaxHighlighting = { enable = true; };
-      oh-my-zsh = {
-        enable = true;
-        plugins = [ "z" "extract" "fd" "fzf" "git" ];
-      };
-      shellAliases = {
-        vim = "nvim";
-        cat = "bat";
-        kg = "kubectl get";
-        kd = "kubectl describe";
-        ke = "kubectl edit";
-        kl = "kubectl logs";
-        lz = "lazygit";
-      };
+    shellAliases = {
+      vim = "nvim";
+      cat = "bat";
+      kg = "kubectl get";
+      kd = "kubectl describe";
+      ke = "kubectl edit";
+      kl = "kubectl logs";
+      lz = "lazygit";
     };
-    programs.fzf = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-    programs.bat.enable = true;
   };
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+  programs.bat.enable = true;
 }
