97a0dafe00a65dd72d8eea9824c51a7c5a1e0ae3
yzld2002 <yzld2002@gmail.com>
2024-01-28 15:20:40 +0800

    kitty

--- home/modules/kitty.nix
+++ home/modules/kitty.nix
@@ -2,7 +2,7 @@
   programs.kitty = {
     enable = true;
     font = {
-      name = "Jetbrains Mono";
+      name = "CaskaydiaCoveNerdFont";
       size = 12;
     };
 
@@ -24,20 +24,12 @@
       enable_audio_bell = false;
     };
     keybindings = {
-      "ctrl+pgdn" = "next_tab";
-      "ctrl+pgup" = "previous_tab";
-      "ctrl+shift+pgdn" = "move_tab_forward";
-      "ctrl+shift+pgup" = "move_tab_backward";
-      "ctrl+shift+t" = "new_tab";
-      "ctrl+w" = "close_tab";
-      "alt+j" = "previous_window";
-      "alt+k" = "next_window";
       "alt+1" = "goto_tab 1";
       "alt+2" = "goto_tab 2";
       "alt+3" = "goto_tab 3";
       "alt+4" = "goto_tab 4";
       "alt+5" = "goto_tab 5";
     };
-    theme = "Gruvbox Material Dark Medium";
+    theme = "One Half Dark";
   };
 }
