b2f1451d6053bcb49b4d4341bb6af5689546ab06
yzld2002 <yzld2002@gmail.com>
2024-02-11 15:58:54 +0800

    traefik module

---
+++ hosts/ms04-nix/modules/traefik/default.nix
@@ -0,0 +1,57 @@
+{ config, ... }:
+{
+  systemd.services.traefik.environment = {
+    TENCENTCLOUD_SECRET_ID = "AKIDHVGGCzdAd6Wmu3Dc2KpHZjnFYfZIzZy9";
+    TENCENTCLOUD_SECRET_KEY = "cat ${config.age.secrets.traefik.dnspod.path}";
+  };
+  services.traefik = {
+    enable = true;
+    staticConfigOptions = {
+      api = {
+        dashboard = true;
+      };
+      entryPoints = {
+        https = {
+          address = ":443";
+          http = {
+            tls = {
+              certResolver = "tencentcloud";
+              domains = [{
+                main = "yzld2002.cn";
+                sans = ["*.yzld2002.cn"];
+              }];
+            };
+          };
+        };
+      };
+      serversTransport = {
+        insecureSkipVerify = true;
+      };
+      certificatesResolvers = {
+        tencentcloud = {
+          acme = {
+            email = "yzld2002@gmail.com";
+            storage = "/var/lib/traefik/acme.json";
+            dnsChallenge = {
+              provider = "tencentcloud";
+            };
+          };
+        };
+      };
+      # providers = {
+      #   kubernetesIngress = {
+      #     endpoint = "https://127.0.0.1:6443";
+      #     # kubectl describe secrets traefik-manual-token
+      #     token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IlNzcE5hck1oY2pRcjU2UE5qb2VQR3pmR2plaXptY3lodGlyZTctaGxfVGsifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6InRyYWVmaWstbWFudWFsLXRva2VuIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6InRyYWVmaWstYWNjb3VudCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6IjgyNjhkOWQ4LWExMTgtNDUxMy1iMWQxLWYxMDhlMzEwYzE5OCIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZWZhdWx0OnRyYWVmaWstYWNjb3VudCJ9.DgjAjlFNbGnTYWnR_Q2k4zZSZ1p47L_MZrjTM84Ng_yll40uNUeI4b91e_Fr1ZW3sDlgdl-0hxtBy3fofZCM8Zk1WfHKCil9jWTfBnAOOF3mycZzAK7j5_9y-BgK0q2nUllatew--SiCLqFW4xwFmeNvXurKIVGTW0myg7XQFKzbpbYQSiO0WdivhjQa-Um2dlrHnWetWl6ENxI0g-to80ltRKLjrn8mVKy1Cv1CEhPL5aIlIjh9O022Ama6O2cki_G1Al750_0xd0a7WSGvaiqPxMq35wBgkJgWNK-4mNFS-o1VvBwwHpzlxJjLSe16eTkBt0r190qoKZ8Umn-ZlQ";
+      #     # cp /var/lib/rancher/k3s/server/tls/server-ca.crt /var/lib/traefik && chown traefik:traefik /var/lib/traefik/server-ca.crt
+      #     certAuthFilePath = "/var/lib/traefik/server-ca.crt";
+      #     ingressEndpoint = {
+      #       hostname = "yzld2002.cn";
+      #       ip = "192.168.50.21";
+      #     };
+      #   };
+      # };
+    };
+    dynamicConfigFile = ./dynamic_conf.yaml;
+  };
+}
---
+++ secrets/secrets.nix
@@ -0,0 +1,2 @@
+let
+  yzld2002 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG3dHygJrJv/ZqC1Y8NhpghQK4hMTuacdJhcXtvfG7dN yzld2002@gmail.com"
