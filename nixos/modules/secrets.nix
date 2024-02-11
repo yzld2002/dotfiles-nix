70c8607bc0b0c2816f104544d3d02b39a38e03ce
yzld2002 <yzld2002@gmail.com>
2024-02-11 15:59:07 +0800

    agenix

--- nixos/modules/secrets.nix
+++ nixos/modules/secrets.nix
@@ -17,28 +17,11 @@ in {
   config = {
     age = {
       identityPaths = [
-        "/home/martijn/.ssh/id_ed25519"
+        "/home/yzld2002/.ssh/id_ed25519"
       ];
       secrets = {
-        hosts = {
-          file = ../../secrets/hosts.age;
-          owner = config.users.users.martijn.name;
-        };
-        password.file = ../../secrets/password.age;
-        smb.file = ../../secrets/smb.age;
-        openai.file = ../../secrets/openai.age;
-        borg.file = ../../secrets/borg.age;
-        transmission.file = ../../secrets/transmission.age;
+        traefik.dnspod.file = ../../secrets/traefik.dnspod.age;
       };
     };
-
-    users.users.martijn.hashedPasswordFile = config.age.secrets.password.path;
-
-    # readFile copies the content into nix-store but only way
-    # to make this work with networking
-    networking.extraHosts =
-      if cfg.hosts
-      then builtins.readFile config.age.secrets.hosts.path
-      else "";
   };
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,2 +1,7 @@
 let
-  yzld2002 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG3dHygJrJv/ZqC1Y8NhpghQK4hMTuacdJhcXtvfG7dN yzld2002@gmail.com"
+  yzld2002 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG3dHygJrJv/ZqC1Y8NhpghQK4hMTuacdJhcXtvfG7dN yzld2002@gmail.com";
+  users = [ yzld2002 ];
+in
+{
+  "traefik.dnspod.age".publicKeys = users;
+}
