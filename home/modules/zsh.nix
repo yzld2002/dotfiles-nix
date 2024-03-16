a8826e9a9939edcd049d06a0a771be6206cab7ad
yzld2002 <yzld2002@gmail.com>
2024-03-17 00:49:17 +0800

    update flake

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -6,7 +6,7 @@
   };
   programs.zsh = {
     enable = true;
-    enableAutosuggestions = true;
+    autosuggestion.enable = true;
     syntaxHighlighting = { enable = true; };
     oh-my-zsh = {
       enable = true;
