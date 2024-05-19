a1fc00b4cd79f69f1370e2bdebc3960ee27a78e8
yzld2002 <yzld2002@gmail.com>
2024-05-19 16:49:28 +0800

    awesome config

--- home/modules/awesomewm.nix
+++ home/modules/awesomewm.nix
@@ -1,7 +1,35 @@
-{ lib, osConfig, ... }:
+{ lib, pkgs, osConfig, ... }:
 with lib; let
   cfg = osConfig.hosts.desktop;
 in {
   config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      polybar
+    ];
+    services.picom = {
+      enable = true;
+      backend = "glx";
+      fade = true;
+      fadeSteps = [0.1 0.1];
+      vSync = true;
+      shadow = false;
+      settings = {
+        # corners
+        corner-radius = 16;
+        rounded-corners-exclude = [
+          "window_type = 'dock'"
+          "window_type = 'desktop'"
+        ];
+      };
+    };
+    home.file.".xprofile".text = ''
+      exec awesome &
+      exec /home/yzld2002/.config/polybar/launch.sh &
+      exec picom &
+    '';
+    xdg.configFile."polybar/" = {
+      source = ./polybar;
+      recursive = true;
+    };
   };
 }
