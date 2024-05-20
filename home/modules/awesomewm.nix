ea60249bf848d86c6b9c701dd0a2a06797cb5581
yzld2002 <yzld2002@gmail.com>
2024-05-21 06:46:23 +0800

    Revert "cursor theme"
    This reverts commit be9e84e770c5e80ad20a881f0aea333f7a18edfa.

--- home/modules/awesomewm.nix
+++ home/modules/awesomewm.nix
@@ -14,8 +14,6 @@ in {
       exec fcitx5
     '';
 
-    home.file.".icons/default".source = "${pkgs.vanilla-dmz}/share/icons/Vanilla-DMZ-AA";
-
     xdg.configFile."polybar/" = {
       source = ./polybar;
       recursive = true;
