febae89fbf27b2754d6adea0abe672c410fee65e
yzld2002 <yzld2002@gmail.com>
2024-06-09 15:00:43 +0800

    update nvim

--- home/modules/neovim/nvim/lua/plugins/plugins.lua
+++ home/modules/neovim/nvim/lua/plugins/plugins.lua
@@ -1,15 +1,26 @@
 return {
-  "folke/flash.nvim",
-  keys = {
-    -- disable the default flash keymap
-    { "s", mode = { "n", "x", "o" }, false },
-    {
-      "r",
-      mode = { "n", "x", "o" },
-      function()
-        require("flash").jump()
-      end,
-      desc = "Flash",
+  {
+    "folke/flash.nvim",
+    keys = {
+      -- disable the default flash keymap
+      { "s", mode = { "n", "x", "o" }, false },
+      {
+        "r",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
     },
   },
+
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    cmd = "Leet",
+    opts = {
+      lang = "kotlin"
+    },
+  }
 }
