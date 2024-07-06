9390ade7b8d11dfc43e96e2ce0340a760781eb43
yzld2002 <yzld2002@gmail.com>
2024-07-06 21:18:07 +0800

    add prisma

--- home/modules/neovim/nvim/lua/plugins/lang.lua
+++ home/modules/neovim/nvim/lua/plugins/lang.lua
@@ -9,6 +9,7 @@ return {
   { import = "lazyvim.plugins.extras.lang.tailwind" },
   { import = "lazyvim.plugins.extras.lang.terraform" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
+  { import = "lazyvim.plugins.extras.lang.prisma" },
   { import = "lazyvim.plugins.extras.linting.eslint" },
   { import = "lazyvim.plugins.extras.util.dot" },
   { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
