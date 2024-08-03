b09c2d3c66b69c9390b607b4d3244e5b8c9747c6
yzld2002 <yzld2002@gmail.com>
2024-08-03 09:44:43 +0800

    add NIX_LD env when start zsh

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -21,6 +21,9 @@
       kl = "kubectl logs";
       lz = "lazygit";
     };
+    initExtra = ''
+      export NIX_LD=$(nix eval --impure --raw --expr 'let pkgs = import <nixpkgs> {}; NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker"; in NIX_LD')
+    '';
   };
   programs.fzf = {
     enable = true;
