424d2b3718a6d2927f6c3475151b9c5947a0ac56
yzld2002 <yzld2002@gmail.com>
2024-08-24 00:18:10 +0800

    add rust to neovim

--- home/modules/neovim/nvim/lua/plugins/lang.lua
+++ home/modules/neovim/nvim/lua/plugins/lang.lua
@@ -10,6 +10,7 @@ return {
   { import = "lazyvim.plugins.extras.lang.terraform" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
   { import = "lazyvim.plugins.extras.lang.prisma" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
   { import = "lazyvim.plugins.extras.linting.eslint" },
   { import = "lazyvim.plugins.extras.util.dot" },
   { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
