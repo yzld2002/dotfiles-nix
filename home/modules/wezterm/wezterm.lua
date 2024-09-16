85fba43af5369baed46c0c92720b27ef2bf7f274
Zhichao <Zhichao.lin@landing.ai>
2024-09-16 21:43:43 +0800

    add rename function to wezterm

--- home/modules/wezterm/wezterm.lua
+++ home/modules/wezterm/wezterm.lua
@@ -70,6 +70,21 @@ config.keys = {
 	{ key = "j", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-down") },
 	{ key = "k", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-up") },
 	{ key = "l", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-right") },
+	{
+		key = "I",
+		mods = "CTRL|SHIFT",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
 }
 
 return config
