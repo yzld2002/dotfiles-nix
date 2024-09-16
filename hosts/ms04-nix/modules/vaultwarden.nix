875578339e046ffdda62df7aeb323cd1c3a264f0
yzld2002 <yzld2002@gmail.com>
2024-09-16 23:56:40 +0800

    config

---
+++ hosts/ms04-nix/modules/vaultwarden.nix
@@ -0,0 +1,27 @@
+{ cfg
+, lib
+, pkgs
+, ...
+}:
+with lib; let
+  cfg = config.hosts.vaultwarden;
+in
+{
+  options.hosts.vaultwarden = {
+    enable = mkEnableOption "BitwardenRS";
+  };
+
+  config = mkIf cfg.enable {
+    service.vaultwarden = {
+      enable = true;
+      backupDir = "/home/yzld2002/download/vaultwarden";
+      environmentFile = [ "${config.age.secrets.vaultwarden.path}" ];
+      config = {
+        DOMAIN = "https://vaultwarden.yzld2002.cn";
+        SIGNUPS_ALLOWED = true;
+        ROCKET_ADDRESS = "::1";
+      };
+    };
+  };
+
+}
