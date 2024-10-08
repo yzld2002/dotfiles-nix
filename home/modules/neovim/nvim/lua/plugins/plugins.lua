53ddb6edc209d546e08e45ef2c4aa472570f67ff
yzld2002 <yzld2002@gmail.com>
2024-10-08 21:53:50 +0800

    add gitlinker

--- home/modules/neovim/nvim/lua/plugins/plugins.lua
+++ home/modules/neovim/nvim/lua/plugins/plugins.lua
@@ -47,4 +47,46 @@ return {
       },
     },
   },
+  {
+    "linrongbin16/gitlinker.nvim",
+    opts = {
+      config = function()
+        require("gitlinker").setup()
+      end,
+    },
+    keys = {
+      {
+        "<leader>gy",
+        "<cmd>GitLink<cr>",
+        silent = true,
+        noremap = true,
+        desc = "Copy git permlink to clipboard",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>gY",
+        "<cmd>GitLink!<cr>",
+        silent = true,
+        noremap = true,
+        desc = "Open git permlink in browser",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>gb",
+        "<cmd>GitLink blame<cr>",
+        silent = true,
+        noremap = true,
+        desc = "Copy git blame link to clipboard",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>gB",
+        "<cmd>GitLink! blame<cr>",
+        silent = true,
+        noremap = true,
+        desc = "Open git blame link in browser",
+        mode = { "n", "v" },
+      },
+    },
+  },
 }
