5354d7352c3471d37ed7156b789ca5e6c465dfbf
yzld2002 <yzld2002@gmail.com>
2024-10-08 21:41:46 +0800

    enable inline gitsigns by default

--- home/modules/neovim/nvim/lua/plugins/plugins.lua
+++ home/modules/neovim/nvim/lua/plugins/plugins.lua
@@ -33,4 +33,18 @@ return {
       { "<C-j>", mode = { "n", "t" }, "<CMD>NavigatorDown<CR>" },
     },
   },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 0,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+        use_focus = true,
+      },
+    },
+  },
 }
